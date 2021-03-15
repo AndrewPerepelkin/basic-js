9ddaa4f28cd374cc25573c07ad7d4b0bac0b6129
AndrewPerepelkin <avp.tep@rambler.ru>
2021-03-15 18:33:31 +0300

    init: added solution of three tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let cats = [];
+
+  matrix.forEach(arr => {
+    arr.filter(item => {
+      if (item === '^^') {
+        cats.push(item);
+      }
+    })
+  });
+  return cats.length;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let arr = [];
+
+  members.forEach(str => {
+    if (typeof (str) === "string") {
+      let member = str.trim().slice(0, 1).toUpperCase();
+      arr.push(member);
+    }
+  });
+
+  let coolSecretName = arr.sort().join('');
+
+  if (coolSecretName === '') {
+    return false;
+  }
+  return coolSecretName;
+
 };
