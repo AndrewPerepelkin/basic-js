1a440ece5d4976d495037b9bfbfbea842d4341da
AndrewPerepelkin <avp.tep@rambler.ru>
2021-09-08 02:13:02 +0300

    feat: add solution vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,86 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mod) {
+    if (typeof mod === 'boolean') {
+      this.reverce = mod;
+    } else {
+      this.reverce = true;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+
+    if (!(message && key)) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let keyRep = key;
+    if (message.length > key.length) {
+      const n = Math.ceil(message.length / key.length)
+      keyRep = key.repeat(n);
+    }
+
+    const k = keyRep.toUpperCase().split('');
+    let countNotChar = 0;
+    const result = message.toUpperCase().split('').map((char, i, message) => {
+      let chari = alphabet.indexOf(char);
+      if (chari === -1) {
+        countNotChar += 1;
+        return message[i] = char;
+      } else {
+        let keyi = alphabet.indexOf(k[i - countNotChar]);
+
+        if ((chari + keyi) <= alphabet.length - 1) {
+          return message[i] = alphabet[chari + keyi];
+        } else {
+          return message[i] = alphabet[(chari + keyi) - alphabet.length];
+        }
+      }
+    });
+
+    if (!this.reverce) {
+      return result.reverse().join('');
+    } else {
+      return result.join('');
+    }
+  }
+
+
+  decrypt(encryptedMessage, key) {
+    if (!(encryptedMessage && key)) {
+      throw new Error('Incorrect arguments!');
+    }
+    let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let keyRep = key;
+    if (encryptedMessage.length > key.length) {
+      let n = Math.ceil(encryptedMessage.length / key.length)
+      keyRep = key.repeat(n);
+    }
+
+    const k = keyRep.toUpperCase().split('');
+    let countNotChar = 0;
+    let result = encryptedMessage.toUpperCase().split('').map((char, i, encryptedMessage) => {
+      let chari = alphabet.indexOf(char);
+      if (chari === -1) {
+        countNotChar += 1;
+        return encryptedMessage[i] = char;
+      } else {
+        let keyi = alphabet.indexOf(k[i - countNotChar]);
+
+        if ((chari - keyi) >= 0) {
+          return encryptedMessage[i] = alphabet[chari - keyi];
+        } else {
+          return encryptedMessage[i] = alphabet[(chari - keyi + alphabet.length)];
+        }
+      }
+    });
+
+    if (!this.reverce) {
+      return result.reverse().join('');
+    } else {
+      return result.join('');
+    }
   }
 }
