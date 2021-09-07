2179b78d4cbd4a80febc6123ea424acc46fd3638
AndrewPerepelkin <avp.tep@rambler.ru>
2021-09-08 01:39:28 +0300

    feat: add solution carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (typeof (sampleActivity) !== 'string') {
+    return false;
+  }
+  const N = parseFloat(sampleActivity);
+  if (isNaN(N) || N <= 0 || N > MODERN_ACTIVITY) {
+    return false;
+  }
+
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const t = Math.ceil(Math.log(MODERN_ACTIVITY / N) / k);
+  return t;
 }
